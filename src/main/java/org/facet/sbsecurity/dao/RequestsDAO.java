af48da1c8a6d2e865ddf7ca7b4aaa736d3e0e1e0
Ustinov Ivan <ustinovis@kamatech.ru>
2019-03-16 12:42:01 +0300

    Изменена страница поданных заявок

--- src/main/java/org/facet/sbsecurity/dao/RequestsDAO.java
+++ src/main/java/org/facet/sbsecurity/dao/RequestsDAO.java
@@ -17,7 +17,8 @@ public class RequestsDAO extends JdbcDaoSupport {
     static Logger logger = Logger.getLogger(RequestsDAO.class);
 
     private String sql;
-    private static final String REQUEST_INSERT = "insert into stankin_db.requests (ticket_from, request_type, application_date, request_status) values (";
+    private static final String REQUEST_INSERT = "insert into stankin_db.requests " +
+            "(ticket_from, request_type, application_date, request_status) values (";
     //private static final String REQUEST_SELECT;
     @Autowired
     public RequestsDAO(DataSource dataSource) {
@@ -59,10 +60,10 @@ public class RequestsDAO extends JdbcDaoSupport {
 
     //Запрос в базу на получение списка заявок по полученному номеру студенческого
     public List<Map<String, Object>> getMyRequests(String ticket) {
-        String sql = "select t1.description as Request, t2.description as Status " +
+        String sql = "select t1.description as Request, t3.application_date as Date, t2.description as Status " +
                 "from stankin_db.TYPES_REQUESTS as t1, stankin_db.STATUS_REQUESTS as t2, " +
                 "stankin_db.REQUESTS as t3 where t1.id = t3.request_type and t2.id = t3.request_status " +
-                "and t3.ticket_from = " + ticket + ";";
+                "and t3.ticket_from = " + ticket + " order by application_date desc;";
 
         try {
             return this.getJdbcTemplate().queryForList(sql);
